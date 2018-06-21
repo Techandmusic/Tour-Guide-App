a91a084377547be25d69e44b138cb4b6e09bba87
Adam <datriphopper@gmail.com>
2018-06-20 22:52:54 -0500

    Added basic constructor to the Cards class

---
+++ app/src/main/java/com/example/android/tourguidepensacola/Cards.java
@@ -0,0 +1,15 @@
+package com.example.android.tourguidepensacola;
+
+public class Cards {
+    private String name;
+    private String about;
+    private int imageID;
+
+    Cards(String cardName, String cardAbout, int cardImage) {
+        name = cardName;
+        about = cardAbout;
+        imageID = cardImage;
+    }
+
+
+}
