9e62f02123d4b6b2f406d3cead1148c2fd166d3f
Adam <datriphopper@gmail.com>
2018-06-20 23:18:59 -0500

    Added more to Cards class

--- app/src/main/java/com/example/android/tourguidepensacola/Cards.java
+++ app/src/main/java/com/example/android/tourguidepensacola/Cards.java
@@ -1,16 +1,38 @@
 package com.example.android.tourguidepensacola;
 
 public class Cards {
-    private String name;
-    private String about;
-    private int imageID;
+
+    //Name of the location on the card
+    private String cardName;
+
+    //Info about the location on the card
+    private String cardAbout;
+
+    //Resource ID for the imag on the card
+    private int cardImageID = NO_IMAGE_PROVIDED;
+
+    //Constant value representing that no image was supplied for this card
+    //May be removed if not needed
+    private static final int NO_IMAGE_PROVIDED -1;
 
     //Class constructor
-    Cards(String cardName, String cardAbout, int cardImage) {
-        name = cardName;
-        about = cardAbout;
-        imageID = cardImage;
+    Cards(String name, String about, int imageResourceID) {
+        cardName = name;
+        cardAbout = about;
+        cardImageID = imageResourceID;
     }
+
+    //getter methods for accessing private variables
+    public getCardName() { return cardName; }
+
+    public getCardAbout() { return cardAbout; }
+
+    public getImageResourceID() { return cardImageID; }
+
+    //Returns whether not an image is supplied for this card
+    //May be removed later if not needed
+    public boolean hasImage() { return cardImageID!= NO_IMAGE_PROVIDED;}
+
 
 
 }
