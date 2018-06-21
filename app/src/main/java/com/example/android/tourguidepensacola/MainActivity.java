86cc7fdbfbb5c472e6c059c80acfa4025b488aeb
Adam <datriphopper@gmail.com>
2018-06-20 23:27:32 -0500

    Updated TODO list

--- app/src/main/java/com/example/android/tourguidepensacola/MainActivity.java
+++ app/src/main/java/com/example/android/tourguidepensacola/MainActivity.java
@@ -12,10 +12,10 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
-// TODO Add cardView xml file for list items
+// TODO Add cardView xml file for list items (Done)
 // TODO Add RecyclerView to hold CardViews
 // TODO Create Fragments for lists
 // TODO Create ViewPager
 // TODO Create Tabs and tabView for navigation
-// TODO Create Location class to store information for cards (custom object)
-// TODO Create custom adapter to populate cards from instances fo the class
+// TODO Create Location class to store information for cards (custom object) (Done)
+// TODO Create custom adapter to populate cards from instances for the class
